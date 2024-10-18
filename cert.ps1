3c8ca4276d7c5267cd5ce850c4c37bfa664ce4e9
Anderson B Silva (Ander) <32654298+oanderoficial@users.noreply.github.com>
2024-10-18 13:00:39 -0300

    Add files via upload

---
+++ cert.ps1
@@ -0,0 +1,13 @@
+# Configura a política de execução para a sessão atual
+Set-ExecutionPolicy RemoteSigned -Scope Process -Force
+
+$certificado = "Cert:\LocalMachine\Root\AD7E1C28B064EF8F6003402014C3D0E3370EB58A"
+$logsave = "C:\Zabbix\scripts\cert.log"
+ 
+$comando = Get-Item $certificado
+$expirar = $comando.NotAfter
+$diasrestantes = ($expirar - (Get-Date)).Days
+ 
+$retorna = $diasrestantes
+ 
+Set-Content -Path $logsave -Value $retorna
